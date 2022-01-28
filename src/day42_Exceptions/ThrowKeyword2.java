190033a06a0b01d6ef590c16583d831a35eaecd1
iryna <iryna.dubenets@yahoo.com>
2022-01-28 08:51:32 -0600

    exceptions practice

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,16 @@
+package day42_Exceptions;
+
+import day31_Constructors.Rectangle;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+    public static void main(String[] args) {
+
+        throw new NoSuchElementException("There is no such an element.");
+
+        //throw new Rectangle(31,35);
+
+
+    }
+}
