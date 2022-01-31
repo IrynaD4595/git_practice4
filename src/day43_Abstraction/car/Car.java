902f871c99757ddf782a1a43565dfabe29936b16
iryna <iryna.dubenets@yahoo.com>
2022-01-31 11:49:27 -0600

    Abstraction starts

--- src/day43_Abstraction/car/Car.java
+++ src/day43_Abstraction/car/Car.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.car;
 
-public class Car {
+public abstract class Car {
 
     private final String brand, model;
     private String color;
@@ -49,6 +49,12 @@ public class Car {
         }
         this.price = price;
     }
+
+    public void stop(){
+        System.out.println("Press the brake");
+    }
+
+    public abstract void start();
 
 
 
