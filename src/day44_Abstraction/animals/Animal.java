4b8c9b02df2b5a14a42a2ed9ef12172a9971516f
iryna <iryna.dubenets@yahoo.com>
2022-02-03 11:10:12 -0600

    Dog claas is completed

--- src/day44_Abstraction/animals/Animal.java
+++ src/day44_Abstraction/animals/Animal.java
@@ -1,5 +1,13 @@
 package day44_Abstraction.animals;
 
+/*
+class Test{
+    public static void main(String[] args) {
+        System.out.println(new Test().getClass().getSimpleName());
+        System.out.println(new Test().getClass().getName());
+    }
+}
+*/
 public abstract class Animal {
 
     private String name;
