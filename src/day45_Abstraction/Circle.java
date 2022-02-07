ca86885bd1b903056364527b2ec013acce575ffc
iryna <iryna.dubenets@yahoo.com>
2022-02-07 14:10:42 -0600

    Circle class is done

---
+++ src/day45_Abstraction/Circle.java
@@ -0,0 +1,38 @@
+package day45_Abstraction;
+
+public class Circle extends Shape{
+
+    private double radius;
+    public final static double pi = 3.14;
+
+    public Circle(double radius) {
+        super("Circle");
+        this.radius= radius;
+    }
+
+    public double getRadius (){
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return radius*radius*pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*radius*pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                super.toString() +
+                "radius=" + radius +
+                '}';
+    }
+}
