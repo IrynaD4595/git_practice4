d3fdc5b3d9eb8c0553e1089757c0626b3d6d1d37
iryna <iryna.dubenets@yahoo.com>
2022-02-07 13:29:19 -0600

    practice abstraction

---
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -0,0 +1,35 @@
+package day45_Abstraction;
+
+public interface PropertiesOfInterface {
+
+    int a = 100;
+    static int b = 200;
+
+    /*
+    public PropertiesOfInterface(int a){
+        this.a = a;
+    }
+*/
+
+    /*
+    static {
+        b = 100;
+    }
+     */
+
+    /*
+    public void method1(){
+        System.out.println("Instance method");
+    }
+*/
+
+    public static void method2(){
+        System.out.println("Static method");
+    }
+
+    public abstract void method3();
+
+    default void method(){
+        System.out.println("Default method");
+    }
+}
