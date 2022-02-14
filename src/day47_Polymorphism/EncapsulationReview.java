18f014f998c1b74e024fc1a1a522e58c000f7852
iryna <iryna.dubenets@yahoo.com>
2022-02-14 14:00:04 -0600

    Encapsulation review is done

---
+++ src/day47_Polymorphism/EncapsulationReview.java
@@ -0,0 +1,27 @@
+package day47_Polymorphism;
+
+public class EncapsulationReview {
+
+    private String bookTitle;
+    private final String publishDate;
+
+    public EncapsulationReview(String bookTitle, String publishDate) {
+        this.bookTitle = bookTitle;
+        this.publishDate = publishDate;
+    }
+
+    public String getBookTitle(){
+        if(bookTitle == null){
+            return "";
+        }
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle){
+        this.bookTitle = bookTitle;
+    }
+
+    public String getPublishDate(){
+        return publishDate;
+    }
+}
