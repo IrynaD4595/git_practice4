b7811491d20123de0069787bfafa154d4e0be082
iryna <iryna.dubenets@yahoo.com>
2022-02-16 10:26:45 -0600

    Odd or even practice is done

--- src/edisonTasks/OddOrEven.java
+++ src/edisonTasks/OddOrEven.java
@@ -3,13 +3,16 @@ package edisonTasks;
 public class OddOrEven {
     public static void main(String[] args) {
 
-        int num = 3;
-            if (num % 2 == 0) {
-                System.out.println(num + " is even!");
-            } else {
-                System.out.println(num + " is odd!");
-            }
 
+        for (int i = 1; i <= 50 ; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i + " is even!");
+
+            } else {
+                System.out.println(i + " is odd!");
+            }
+        }
 
 
 
